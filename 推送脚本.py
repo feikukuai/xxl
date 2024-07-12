cccb7ae77fcfa0f51c6583fee042d8f5ba27378b
root <root@localhost.localdomain>
2024-07-12 04:22:23 +0000

    正确的版本，测试999

--- 推送脚本.py
+++ 推送脚本.py
@@ -8,14 +8,14 @@ def run_command(command):
         print(f"An error occurred: {e.stderr}")
 
 # 初始化Git仓库
-run_command('git pull origin main')
-run_command('git init')
+#run_command('git pull origin main')
+#run_command('git init')
 
 # 添加所有文件到暂存区
 run_command('git add .')
 
 # 提交更改
-run_command('git commit -m "正确的版本，测试"')
+run_command('git commit -m "正确的版本，测试999"')
 
 # 添加远程仓库
 run_command('git remote add origin git@github.com:feihukuai/word.git')
@@ -23,6 +23,6 @@ run_command('git remote add origin git@github.com:feihukuai/word.git')
 # 推送到远程仓库
 # 注意：这里没有使用 -u 参数，如果需要可以添加
 run_command('git push -u origin main --force')
-
+run_command('git push origin HEAD:main')
 # 如果你使用的是 'main' 分支，请使用以下命令
 # run_command('git push origin main')
