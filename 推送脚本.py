0d2b4113265b7eed73d94f825ee52acfa1432129
root <root@localhost.localdomain>
2024-07-12 03:20:14 +0000

    正确的版本，测试99

--- 推送脚本.py
+++ 推送脚本.py
@@ -15,10 +15,10 @@ run_command('git init')
 run_command('git add .')
 
 # 提交更改
-run_command('git commit -m "正确的版本，测试"')
+run_command('git commit -m "正确的版本，测试99"')
 
 # 添加远程仓库
-run_command('git remote add origin git@github.com:feihukuai/word.git')
+#run_command('git remote add origin git@github.com:feihukuai/word.git')
 
 # 推送到远程仓库
 # 注意：这里没有使用 -u 参数，如果需要可以添加
