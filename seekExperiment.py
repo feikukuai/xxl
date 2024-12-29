720a4cb8c9bcacf4afa0aff2ff0fbcb609ddf41a
root <root@localhost.localdomain>
2024-12-29 22:12:29 +0000

    deepseekExperiment

--- seekExperiment.py
+++ seekExperiment.py
@@ -2,7 +2,7 @@
 
 from openai import OpenAI
 
-client = OpenAI(api_key="<DeepSeek API Key>", base_url="https://api.deepseek.com")
+client = OpenAI(api_key="sk-7b07c77962f445e88eb184369d0e49f2", base_url="https://api.deepseek.com")
 
 response = client.chat.completions.create(
     model="deepseek-chat",
